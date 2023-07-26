d65f8daef354d317abe1ff4362661a9d153b647f
harsh796719 <96861098+harsh796719@users.noreply.github.com>
2023-07-26 10:35:23 +0530

    Update pr2.js

--- pr2.js
+++ pr2.js
@@ -1,57 +1,51 @@
 let numbersArray = [1, 2, 3, 4, 5];
 
 console.log("Length of the array:", numbersArray.length);
-
 console.log("Element at index 2:", numbersArray[2]);
 console.log("Element at index 4:", numbersArray[4]);
-
-numbersArray.push(6); // Add an element at the end
+numbersArray.push(6); 
 console.log("After push(6):", numbersArray);
-
-numbersArray.pop(); // Remove the last element
+numbersArray.pop(); 
 console.log("After pop():", numbersArray);
 
-numbersArray.shift(); // Remove the first element
+numbersArray.shift();
 console.log("After shift():", numbersArray);
 
-numbersArray.unshift(0); // Add an element at the beginning
+numbersArray.unshift(0);
 console.log("After unshift(0):", numbersArray);
 
-delete numbersArray[2]; // Delete an element (leaves undefined at that index)
+delete numbersArray[2];
 console.log("After delete:", numbersArray);
-// console.log("numbersArray[2]", numbersArray[2]);
 
-const joinedArray = numbersArray.join("-"); // Join elements with "-"
+const joinedArray = numbersArray.join("-");
 console.log("Joined Array:", joinedArray);
 
 const otherArray = [7, 8, 9];
-const concatenatedArray = numbersArray.concat(otherArray); // Concatenate two arrays
+const concatenatedArray = numbersArray.concat(otherArray); 
 console.log("Concatenated Array:", concatenatedArray);
 
-const splicedArray = numbersArray.splice(2, 2, 10, 11); // Remove 2 elements starting from index 1 and add 10, 11
+const splicedArray = numbersArray.splice(2, 2, 10, 11);
 console.log("After splice():", numbersArray);
 
-const slicedArray = concatenatedArray.slice(1, 4); // Extract elements from index 1 to index 3 (4-1)
+const slicedArray = concatenatedArray.slice(1, 4);
 console.log("Sliced Array:", slicedArray);
 
 const flatArray = [1, [2, 3], [4, [5]]];
-console.log("Flat Array:", flatArray.flat()); // Flatten nested arrays
+console.log("Flat Array:", flatArray.flat());
 
 
-// Object representing a person
-console.log("person object");
 const person = {
     name: "Harsh Patel",
     age: 19,
     gender: "Male"
 };
 
-// Function to display person's details
+
 function displayPersonDetails(personObj) {
     console.log("Name:", personObj.name);
     console.log("Age:", personObj.age);
     console.log("Gender:", personObj.gender);
 }
 
-// Display person's details
+
 displayPersonDetails(person);
