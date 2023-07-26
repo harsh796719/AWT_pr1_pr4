79ebc5a7cdb9b1011d9b116c3e9103338dc35fa0
harsh796719 <96861098+harsh796719@users.noreply.github.com>
2023-07-26 10:39:21 +0530

    Update pr3.js

--- pr3.js
+++ pr3.js
@@ -1,98 +1,62 @@
-// 1. let
 let x = 10;
 if (true) {
-    let x = 20; // This creates a new variable x inside the block
-    console.log(x); // Output: 20
+    let x = 20;
+    console.log(x);
 }
-console.log(x); // Output: 10
+console.log(x);
 
-
-// 2. const
 const PI = 3.14;
-// PI = 3.1415; // Error: Assignment to constant variable.
-
-const colors = ["red", "green", "blue"];
-// colors = ["yellow", "orange"]; // Error: Assignment to constant variable.
-colors.push("yellow"); // Allowed, as the array is mutable.
-console.log(colors); // Output: ["red", "green", "blue", "yellow"]
+// PI = 3.1415; error
 
 
-// 3. Arrow fuction
-
-// Regular function
-function add(a, b) {
-    return a + b;
-}
-
-// Arrow function
 const addArrow = (a, b) => a + b;
-
-console.log(add(2, 3)); // Output: 5
-console.log(addArrow(2, 3)); // Output: 5
-
-
-// 4. Spread of operator
+console.log(addArrow(2, 3));
 
 const numbers = [1, 2, 3];
 const newNumbers = [...numbers, 4, 5];
 
-console.log(newNumbers); // Output: [1, 2, 3, 4, 5]
-
-// 5. for/of loop
+console.log(newNumbers);
 
 const cs = ["red", "green", "blue"];
-
 for (const color of cs) {
     console.log(color);
 }
 
-// 6. map object
-
 const myMap = new Map();
-
 myMap.set("name", "Harsh");
 myMap.set("age", 19);
 
-console.log(myMap.get("name")); // Output: "Harsh"
-console.log(myMap.get("age")); // Output: 19
+console.log(myMap.get("name"));
+console.log(myMap.get("age"));
 
-
-// 7. set object
 const mySet = new Set();
-
 mySet.add(1);
 mySet.add(2);
-mySet.add(2); // Duplicates are ignored
+mySet.add(2);
+console.log(mySet);
 
-console.log(mySet); // Output: Set { 1, 2 }
 
-// 8. classes
 class Person {
-    constructor(name, age) {
+    constructor(name) {
         this.name = name;
-        this.age = age;
     }
 
-    sayHello() {
-        console.log(`Hello, my name is ${this.name}, and I am ${this.age} years old.`);
+    sHello() {
+        console.log(`Hii, my name is ${this.name}`);
     }
 }
 
-const hp = new Person("Harsh", 19);
-hp.sayHello(); // Output: "Hello, my name is harsh, and I am 19 years old."
+const hp = new Person("Harsh");
+hp.Hello();
 
-
-// 9. promises
 
 function fetchData() {
     return new Promise((resolve, reject) => {
-        // Simulating async operation
+       
         setTimeout(() => {
             const data = "Resolved data";
             // resolve the promise with the data
             resolve(data);
-            // or reject the promise with an error
-            // reject("Error occurred");
         }, 2000);
     });
 }
@@ -102,23 +66,17 @@ fetchData()
     .catch((error) => console.error(error));
 
 
-// 10. symbol
-
 const id1 = Symbol("id");
 const id2 = Symbol("id");
 
-console.log(id1 === id2); // Output: false
-
-// 11. default parameters
+console.log(id1 === id2); 
 
 function greet(name = "Anonymous") {
     console.log(`Hello, ${name}!`);
 }
 
-greet(); // Output: "Hello, Anonymous!"
-greet("John"); // Output: "Hello, John!"
-
-// 12. fuction rest parameter
+greet();
+greet("harshh");
 
 function sum(...numbers) {
     return numbers.reduce((acc, curr) => acc + curr, 0);
