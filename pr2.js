920036a0f9245fe334e754806b18670545a83d9b
harsh796719 <96861098+harsh796719@users.noreply.github.com>
2023-07-26 10:58:06 +0530

    Update pr2.js

--- pr2.js
+++ pr2.js
@@ -1,51 +1,45 @@
-let numbersArray = [1, 2, 3, 4, 5];
+let array = [1, 2.1 , "harsh", 4, 5];
 
-console.log("Length of the array:", numbersArray.length);
-console.log("Element at index 2:", numbersArray[2]);
-console.log("Element at index 4:", numbersArray[4]);
-numbersArray.push(6); 
-console.log("After push(6):", numbersArray);
-numbersArray.pop(); 
-console.log("After pop():", numbersArray);
+console.log("length of the array:", array.length);
+array.push(6); 
+console.log("after push(6):", array);
+array.pop(); 
+console.log("pop():", array);
 
-numbersArray.shift();
-console.log("After shift():", numbersArray);
+array.shift();
+console.log("shift():", array);
 
-numbersArray.unshift(0);
-console.log("After unshift(0):", numbersArray);
+array.unshift(0);
+console.log("unshift(0):", array);
 
-delete numbersArray[2];
-console.log("After delete:", numbersArray);
+delete array[2];
+console.log("delete:", array);
 
-const joinedArray = numbersArray.join("-");
-console.log("Joined Array:", joinedArray);
+const jArray = array.join("-");
+console.log("joined Array:", jArray);
 
-const otherArray = [7, 8, 9];
-const concatenatedArray = numbersArray.concat(otherArray); 
-console.log("Concatenated Array:", concatenatedArray);
+const oArray = [7, 8, 9];
+const cArray = array.concat(oArray); 
+console.log("Concatenat Array:", cArray);
 
-const splicedArray = numbersArray.splice(2, 2, 10, 11);
-console.log("After splice():", numbersArray);
+const sArray = array.splice(2, 2, 10, 11);
+console.log("After splice():", array);
 
-const slicedArray = concatenatedArray.slice(1, 4);
-console.log("Sliced Array:", slicedArray);
+const sArray = cArray.slice(1, 4);
+console.log("Slice():", sArray);
 
-const flatArray = [1, [2, 3], [4, [5]]];
-console.log("Flat Array:", flatArray.flat());
+const temp = [1, [2, 3], [4]];
+console.log("flat():", temp.flat());
 
 
 const person = {
     name: "Harsh Patel",
     age: 19,
-    gender: "Male"
 };
 
 
-function displayPersonDetails(personObj) {
-    console.log("Name:", personObj.name);
-    console.log("Age:", personObj.age);
-    console.log("Gender:", personObj.gender);
+function display(obj) {
+    console.log("name:", obj.name+" age : "+"obj.age");
 }
 
-
-displayPersonDetails(person);
+display(person);
