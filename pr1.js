8bc06e67a1733b99f95c1ef8d266013b9ebdf6d9
harsh796719 <96861098+harsh796719@users.noreply.github.com>
2023-07-26 10:47:03 +0530

    Update pr1.js

--- pr1.js
+++ pr1.js
@@ -1,10 +1,10 @@
-const stringValue = "Harsh patel";
-const numberValue = 42;
-const booleanValue = true;
+const sV = "Harsh patel";
+const nV = 42;
+const bV = true;
 
-console.log("String Value:", stringValue);
-console.log("Number Value:", numberValue);
-console.log("Boolean Value:", booleanValue);
+console.log("String Value:", sV);
+console.log("Number Value:", nV);
+console.log("Boolean Value:", bV);
 
 function addNumbers(num1, num2) {
     return num1 + num2;
